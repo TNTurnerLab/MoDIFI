c4ac27ba38292fe2d1b90a0e3adfa92e4f6332d9
arvinden <35161556+ARVINDEN@users.noreply.github.com>
2026-09-17 15:48:07 -0500

    Update baconForDEseq.R
    added seed parameter for bacon function

--- scripts/baconForDEseq.R
+++ scripts/baconForDEseq.R
@@ -5,7 +5,7 @@ library(stringr)
 
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- args[1]
-
+seed <- args[2]
 
 
 file_list <- c(input_file)
@@ -52,7 +52,7 @@ for (in_path in file_list) {
     z_by_gid <- as.numeric(z_by_gid)              # length K
     
     # 3) run bacon on the per-gene Zs
-    set.seed(1234)
+    set.seed(seed)
     bc_u   <- bacon::bacon(teststatistics = z_by_gid)
     adjZ_u <- bacon::tstat(bc_u)
     
